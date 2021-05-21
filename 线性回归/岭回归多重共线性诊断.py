40afc0c466341a0bc5b8091958c61e8579f7c3c7
xuewei <1550505935@qq.com>
2021-05-22 01:48:58 +0800

    examples of linear regression

---
+++ 线性回归/岭回归多重共线性诊断.py
@@ -0,0 +1,17 @@
+# 导入计算膨胀因子的库
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import pandas as pd
+# get_loc(i) 返回对应列名所在的索引
+
+data = pd.read_excel('4.岭回归数据.xlsx')
+
+x= data
+
+# vif>10则说明存在多重共线性，需要处理
+vif=[variance_inflation_factor(x.values,x.columns.get_loc(i)) for i in x.columns]
+
+# 测试x.columns.get_loc(i)的含义，这里为获得不同列名的索引值
+for i in x.columns:
+    print(x.columns.get_loc(i))
+
+print(vif)
