312850b731fbf9119bd7d4732621b9a7806e4745
xuewei <1550505935@qq.com>
2022-01-04 21:04:15 +0800

    commit

--- 线性回归/一元线性回归.py
+++ 线性回归/一元线性回归.py
@@ -2,6 +2,7 @@
 import numpy as np  # 快速操作结构数组的工具
 import matplotlib.pyplot as plt  # 可视化绘制
 from sklearn.linear_model import LinearRegression  # 线性回归
+from sklearn.metrics import r2_score
 
 # 样本数据集，第一列为x，第二列为y，在x和y之间建立回归模型
 data = [
@@ -15,6 +16,7 @@ data = [
 
 # 生成X和y矩阵
 dataMat = np.array(data)
+print(dataMat)
 X = dataMat[:, 0:1]  # 变量x
 y = dataMat[:, 1]  # 变量y
 
@@ -27,6 +29,7 @@ print('线性回归模型:\n', model)
 # 使用模型预测
 predicted = model.predict(X)
 
+print('R2:',r2_score(y,predicted))
 # 绘制散点图 参数：x横轴 y纵轴
 plt.scatter(X, y, marker='x')
 plt.plot(X, predicted, c='r')
